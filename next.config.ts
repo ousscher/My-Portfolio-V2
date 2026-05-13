08d560a1e4d5b827e4bfae9341912e86cbb5c903
ousshcer <lo_cherguelaine@esi.dz>
2026-05-13 22:03:17 +0200

    feat: add redirect for Figma board in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/figma",
+        destination:
+          "https://www.figma.com/board/MRLns48vIyL0O1w15IPOqZ/Untitled?node-id=1-463&t=DjgaXI2TQjNbt2CS-1",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
